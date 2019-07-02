46f589f0b7736d9abeaf41e1b70330eb5f122fea
codemvs <codemvs@gmail.com>
2019-07-02 18:04:03 -0500

    Numeros

--- TypeScript/main.js
+++ TypeScript/main.js
@@ -1,16 +1,24 @@
 "use strict";
 //==== BOOLEANS ==
-var esSuperman = true;
-var esBatman;
-var esAquaman = true;
-if (esSuperman) {
-    console.log('Estamos Salvador');
-}
-else {
-    console.log('Ops');
-}
-esSuperman = convertirClark();
-function convertirClark() {
-    return true;
-}
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIm1haW4udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLGtCQUFrQjtBQUNsQixJQUFJLFVBQVUsR0FBWSxJQUFJLENBQUM7QUFDL0IsSUFBSSxRQUFpQixDQUFDO0FBQ3RCLElBQUksU0FBUyxHQUFXLElBQUksQ0FBQztBQUU3QixJQUFJLFVBQVUsRUFBRztJQUNiLE9BQU8sQ0FBQyxHQUFHLENBQUMsa0JBQWtCLENBQUMsQ0FBQztDQUVuQztLQUFJO0lBQ0QsT0FBTyxDQUFDLEdBQUcsQ0FBQyxLQUFLLENBQUMsQ0FBQztDQUV0QjtBQUVELFVBQVUsR0FBRyxjQUFjLEVBQUUsQ0FBQztBQUU5QixTQUFTLGNBQWM7SUFDbkIsT0FBTyxJQUFJLENBQUM7QUFDaEIsQ0FBQyJ9
+// let esSuperman: boolean = true;
+// let esBatman: boolean;
+// let esAquaman: boolean =true;
+// if( esSuperman ) {
+//     console.log('Estamos Salvador');
+// }else{
+//     console.log('Ops');
+// }
+// esSuperman = convertirClark();
+// function convertirClark() {
+//     return true;
+// }
+// === ENTEROS === 
+// let avengers: number=5;
+// let villanos: number;
+// let otros = 2;
+// if( avengers > villanos ) {
+//     console.log('Salvados');
+// }else{
+//     console.log('Muertos');
+// }
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIm1haW4udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLGtCQUFrQjtBQUNsQixrQ0FBa0M7QUFDbEMseUJBQXlCO0FBQ3pCLGdDQUFnQztBQUVoQyxxQkFBcUI7QUFDckIsdUNBQXVDO0FBRXZDLFNBQVM7QUFDVCwwQkFBMEI7QUFFMUIsSUFBSTtBQUVKLGlDQUFpQztBQUVqQyw4QkFBOEI7QUFDOUIsbUJBQW1CO0FBQ25CLElBQUk7QUFFSixtQkFBbUI7QUFDbkIsMEJBQTBCO0FBQzFCLHdCQUF3QjtBQUN4QixpQkFBaUI7QUFFakIsOEJBQThCO0FBQzlCLCtCQUErQjtBQUUvQixTQUFTO0FBQ1QsOEJBQThCO0FBRTlCLElBQUkifQ==
--- TypeScript/main.ts
+++ TypeScript/main.ts
@@ -1,18 +1,31 @@
 //==== BOOLEANS ==
-let esSuperman: boolean = true;
-let esBatman: boolean;
-let esAquaman: boolean =true;
+// let esSuperman: boolean = true;
+// let esBatman: boolean;
+// let esAquaman: boolean =true;
 
-if( esSuperman ) {
-    console.log('Estamos Salvador');
+// if( esSuperman ) {
+//     console.log('Estamos Salvador');
     
-}else{
-    console.log('Ops');
+// }else{
+//     console.log('Ops');
     
-}
+// }
 
-esSuperman = convertirClark();
+// esSuperman = convertirClark();
 
-function convertirClark() {
-    return true;
-}
+// function convertirClark() {
+//     return true;
+// }
+
+// === ENTEROS === 
+// let avengers: number=5;
+// let villanos: number;
+// let otros = 2;
+
+// if( avengers > villanos ) {
+//     console.log('Salvados');
+    
+// }else{
+//     console.log('Muertos');
+    
+// }
