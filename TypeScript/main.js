80722fba075531b3d0317e8df0d24c308c8fa101
codemvs <codemvs@gmail.com>
2019-07-02 18:01:23 -0500

    Booleanos

---
+++ TypeScript/main.js
@@ -0,0 +1,16 @@
+"use strict";
+//==== BOOLEANS ==
+var esSuperman = true;
+var esBatman;
+var esAquaman = true;
+if (esSuperman) {
+    console.log('Estamos Salvador');
+}
+else {
+    console.log('Ops');
+}
+esSuperman = convertirClark();
+function convertirClark() {
+    return true;
+}
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIm1haW4udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLGtCQUFrQjtBQUNsQixJQUFJLFVBQVUsR0FBWSxJQUFJLENBQUM7QUFDL0IsSUFBSSxRQUFpQixDQUFDO0FBQ3RCLElBQUksU0FBUyxHQUFXLElBQUksQ0FBQztBQUU3QixJQUFJLFVBQVUsRUFBRztJQUNiLE9BQU8sQ0FBQyxHQUFHLENBQUMsa0JBQWtCLENBQUMsQ0FBQztDQUVuQztLQUFJO0lBQ0QsT0FBTyxDQUFDLEdBQUcsQ0FBQyxLQUFLLENBQUMsQ0FBQztDQUV0QjtBQUVELFVBQVUsR0FBRyxjQUFjLEVBQUUsQ0FBQztBQUU5QixTQUFTLGNBQWM7SUFDbkIsT0FBTyxJQUFJLENBQUM7QUFDaEIsQ0FBQyJ9
---
+++ TypeScript/main.ts
@@ -0,0 +1,18 @@
+//==== BOOLEANS ==
+let esSuperman: boolean = true;
+let esBatman: boolean;
+let esAquaman: boolean =true;
+
+if( esSuperman ) {
+    console.log('Estamos Salvador');
+    
+}else{
+    console.log('Ops');
+    
+}
+
+esSuperman = convertirClark();
+
+function convertirClark() {
+    return true;
+}
