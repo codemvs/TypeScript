4a12dcea25d3c50b78e54e6ee347f071a1688ea8
codemvs <codemvs@gmail.com>
2019-07-03 17:58:36 -0500

    Ejercicio resuelto

--- objetos/app.ts
+++ objetos/app.ts
@@ -43,3 +43,76 @@ if( typeof cosa === "number" ){
     console.log('Is number');
     
 }
+
+
+
+// EJERCICIOS
+
+// Objetos
+type Auto = {
+    carroceria: string,
+    modelo: string,
+    antibalas: boolean,
+    pasajeros:number,
+    disparar?:()=>void
+  }
+  
+  let batimovil: Auto = {
+    carroceria: "Negra",
+    modelo: "6x6",
+    antibalas: true,
+    pasajeros:4  
+  };
+  
+  let bumblebee: Auto = {
+    carroceria: "Amarillo con negro",
+    modelo: "4x2",
+    antibalas: true,
+    pasajeros:4,
+    disparar(){ // El metodo disparar es opcional
+      console.log("Disparando");
+    }
+  };
+  
+  
+  // Villanos debe de ser un arreglo de objetos personalizados
+  type Villano = {
+    nombre:string,
+    edad: number | undefined,
+    mutante:boolean
+  }
+  let villanos: Villano[] = [{
+    nombre:"Lex Luthor",
+    edad: 54,
+    mutante:false
+  },{
+    nombre: "Erik Magnus Lehnsherr",
+    edad: 49,
+    mutante: true
+  },{
+    nombre: "James Logan",
+    edad: undefined,
+    mutante: true
+  }];
+  
+  // Multiples tipos
+  // cree dos tipos, uno para charles y otro para apocalipsis
+  type TCharles={poder: string, estatura:number};
+  type TApocalipsis = { lider: boolean,miembros: string[] };
+  
+  let charles: TCharles  = {
+    poder:"psiquico",
+    estatura: 1.78
+  };
+  
+  let apocalipsis: TApocalipsis = {
+    lider:true,
+    miembros: ["Magneto","Tormenta","Psylocke","Angel"]
+  }
+  
+  // Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
+  let mystique: TCharles | TApocalipsis;
+  
+  mystique = charles;
+  mystique = apocalipsis;
+  
