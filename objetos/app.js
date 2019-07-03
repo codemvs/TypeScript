648c64e4b7ee42cef2790814babcf9bcbb406535
codemvs <codemvs@gmail.com>
2019-07-03 16:22:56 -0500

    Objetos

---
+++ objetos/app.js
@@ -0,0 +1,15 @@
+"use strict";
+// OBjetos
+var flash = {
+    nombre: 'Barry Allen',
+    edad: 24,
+    poderes: ['Puede correr myy rapido', 'Viaja por el tiempo'],
+    getNombre: function () {
+        return this.nombre;
+    }
+};
+// flash = {
+//     nombre: 'Piter',
+//     edad: 500,
+//     poderes:['volar']
+// }
---
+++ objetos/app.ts
@@ -0,0 +1,16 @@
+// OBjetos
+let flash: { nombre:string, edad: number, poderes: string[], getNombre:()=>string } = {
+    nombre: 'Barry Allen',
+    edad:24,
+    poderes:['Puede correr myy rapido','Viaja por el tiempo'],
+    getNombre(){
+        return this.nombre;
+    }
+}
+// flash = {
+//     nombre: 'Piter',
+//     edad: 500,
+//     poderes:['volar']
+// }
+
+
