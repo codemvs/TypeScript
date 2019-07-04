43965139f045d239bf9551551a29bd5af78deb33
codemvs <codemvs@gmail.com>
2019-07-04 16:09:51 -0500

    Metodos publicos y privados

--- clases/app.js
+++ clases/app.js
@@ -1,7 +1,7 @@
 "use strict";
 var Avenger = /** @class */ (function () {
     function Avenger(nombre, equipo, nombreReal) {
-        this.nombre = "Default"; // acceder a cuaquier logar
+        this.nombre = "Default"; // acceder a cuaquier lugar
         this.equipo = ""; // Solo se puede cambiar el valor dentro de la clase o su herencia
         this.nombreReal = ""; // Solo pueden ser modifiadas los valores dentro de esta clase
         this.puedePelear = true;
@@ -10,7 +10,19 @@ var Avenger = /** @class */ (function () {
         this.equipo = equipo;
         this.nombreReal = nombreReal;
     }
+    Avenger.prototype.bio = function () {
+        var mensaje = this.nombre + " " + this.nombreReal + " " + this.equipo;
+        console.log(mensaje);
+    };
+    Avenger.prototype.cambiarEquipo = function (nuevoEquipo) {
+        return nuevoEquipo === this.equipo;
+    };
+    Avenger.prototype.cambiaEquipoPublico = function (nuevoEquipo) {
+        return this.cambiarEquipo(nuevoEquipo);
+    };
     return Avenger;
 }());
 var antman = new Avenger('Antman', 'cap', 'Scott Lang');
 antman.nombre = 'Clark kent';
+antman.bio();
+console.log(antman.cambiaEquipoPublico("CAP"));
--- clases/app.ts
+++ clases/app.ts
@@ -1,5 +1,5 @@
 class Avenger {
-    public nombre: string = "Default"; // acceder a cuaquier logar
+    public nombre: string = "Default"; // acceder a cuaquier lugar
     protected equipo: string = ""; // Solo se puede cambiar el valor dentro de la clase o su herencia
     private nombreReal: string = "";   // Solo pueden ser modifiadas los valores dentro de esta clase
     private puedePelear: boolean = true;
@@ -13,9 +13,27 @@ class Avenger {
         this.equipo= equipo;
         this.nombreReal = nombreReal;
     }
+    
+    public bio():void{
+        let mensaje = `${ this.nombre } ${ this.nombreReal } ${this.equipo}`;
+        console.log( mensaje );        
+    }
+
+    
+    private cambiarEquipo(nuevoEquipo: string): boolean {
+        return nuevoEquipo === this.equipo;        
+    }
+
+    public cambiaEquipoPublico( nuevoEquipo: string ) : boolean{
+        return this.cambiarEquipo( nuevoEquipo );
+    }
+
+    
 }
 
 let antman: Avenger = new Avenger('Antman','cap','Scott Lang');
 
 antman.nombre = 'Clark kent';
+antman.bio();
+console.log(antman.cambiaEquipoPublico("CAP"));
 
