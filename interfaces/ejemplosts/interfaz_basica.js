e16f99bd3963af90cd5b764e0f198419a1371412
codemvs <codemvs@gmail.com>
2019-07-05 10:31:33 -0500

    interfaces

--- interfaces/ejemplosts/interfaz_basica.js
+++ interfaces/ejemplosts/interfaz_basica.js
@@ -1,13 +1,17 @@
 "use strict";
 function enviarMision(xmen) {
     console.log('Enviando mision a ' + xmen.nombre);
+    xmen.regenerar('Logan');
 }
 function enviarCuartel(xmen) {
     console.log('Enviando al cuartel ' + xmen.nombre);
+    xmen.regenerar('Logan2');
 }
 var wolverine = {
     nombre: 'Wolverine',
-    poder: 'garras'
+    regenerar: function (x) {
+        console.log('Se ha regenerado ' + x);
+    }
 };
 enviarMision(wolverine);
 enviarCuartel(wolverine);
--- interfaces/ejemplosts/interfaz_basica.ts
+++ interfaces/ejemplosts/interfaz_basica.ts
@@ -1,21 +1,24 @@
 interface Xmen {
     nombre: string,
-    poder: string
+    poder?: string,
+    regenerar( nombreReal: string ): void
 }
 
 function enviarMision( xmen: Xmen ): void {
     console.log('Enviando mision a '+ xmen.nombre);
-    
+    xmen.regenerar('Logan');    
 }
 
 function enviarCuartel( xmen: Xmen ): void{
     console.log('Enviando al cuartel '+ xmen.nombre);
-    
+    xmen.regenerar('Logan2');    
 }
 
 let wolverine: Xmen = {
-    nombre: 'Wolverine',
-    poder:'garras'
+    nombre: 'Wolverine',    
+    regenerar(x:string){
+        console.log( 'Se ha regenerado '+ x );        
+    }
 }
 
 enviarMision(wolverine);
