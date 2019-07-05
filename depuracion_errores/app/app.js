1fceb9a620f0fe7abb072d93b2fe6b73f4014518
codemvs <codemvs@gmail.com>
2019-07-05 15:25:42 -0500

    Namespace

--- depuracion_errores/app/app.js
+++ depuracion_errores/app/app.js
@@ -6,5 +6,7 @@
 function myFuncion(texto) {
     console.log(texto);
 }
+/**
+ * Comentarios
+ */
 myFuncion('Sa');
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiYXBwLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQTs7O0dBR0c7QUFFSCxTQUFTLFNBQVMsQ0FBRSxLQUFhO0lBQzdCLE9BQU8sQ0FBQyxHQUFHLENBQUMsS0FBSyxDQUFDLENBQUM7QUFFdkIsQ0FBQztBQUlELFNBQVMsQ0FBQyxJQUFJLENBQUMsQ0FBQSJ9
